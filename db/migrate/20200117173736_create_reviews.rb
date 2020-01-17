7143b3e76357ec860ca9195bb0881d71887c4e15
Masha Ivy <mashanechmo@gmail.com>
2020-01-17 10:00:06 -0800

    add a migration with a new table

---
+++ db/migrate/20200117173736_create_reviews.rb
@@ -0,0 +1,9 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.column(:author, :string)
+      t.column(:content_body, :text)
+      t.column(:rating, :integer)
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_173152) do
+ActiveRecord::Schema.define(version: 2020_01_17_173736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2020_01_17_173152) do
     t.string "country_of_origin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.text "content_body"
+    t.integer "rating"
   end
 
 end
