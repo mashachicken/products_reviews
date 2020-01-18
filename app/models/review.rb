41d68704e49cf63a34d23b70d5f88433a75d4e72
Masha Ivy <mashanechmo@gmail.com>
2020-01-17 17:35:10 -0800

    add validations and callbacks

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :product
+  before_save(:titleize_album)
+
+private
+  def titleize_album
+    self.author = self.author.titleize
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to(:product) }
+  it { should validate_presence_of :name }
+  it("titleizes the name of the author") do
+    review = Review.create({author: "masha", content_body: "good product", rating: 5})
+    expect(review.author()).to(eq("Masha"))
+  end
 end
